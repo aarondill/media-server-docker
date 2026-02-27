51474e92600feaf89fc2177a7832811c7d7c1f0d
Aaron Dill <aaronsacks2006@gmail.com>
2026-02-27 14:52:31 -0600

    fix: write empty file on no containers

--- tailscale-dns/dnsmasq-generator.sh
+++ tailscale-dns/dnsmasq-generator.sh
@@ -12,29 +12,27 @@ INTERFACE=tailscale0
 go() {
   ids=()
   if ! read -r -a ids -d '' < <(docker container ls --filter "label=$DOMAIN_LABEL" --format '{{ .ID }}' --no-trunc); then
-    echo "No containers found with label $DOMAIN_LABEL"
-    return
+    printf "%s\n" 'No containers found with label %s' "$DOMAIN_LABEL" >&2
+    return 1
   fi
   # Get ip, then remove subnet from the end
   ips=()
   if ! read -r -a ips -d '' < <(ip addr show dev "$INTERFACE" scope global | awk '/inet[0-9]*/{print $2}' | cut -d/ -f1); then
-    echo "No IPs found for $INTERFACE"
-    return
+    printf '%s\n' 'No IPs found for %s' "$INTERFACE" >&2
+    return 1
   fi
-
-  res=()
   for id in "${ids[@]}"; do
     domain=$(docker inspect "$id" -f "{{.Config.Labels.$DOMAIN_LABEL}}")
     for ip in "${ips[@]}"; do
-      # append to res
-      printf -v "res[${#res[@]}]" '%-26s\t%s' "$ip" "$domain"
+      printf '%-26s\t%s\n' "$ip" "$domain"
     done
   done
-  # write all at once to avoid dnsmasq reading partial files
-  printf '%s\n' "${res[@]}" >"$output_file"
 }
 
 while true; do
-  go
+  # write even if it returns 1, since this means we don't want any entries
+  contents=$(go) || true
+  # write all at once to avoid dnsmasq reading partial files
+  printf '%s\n' "$contents" >"$output_file"
   sleep "$interval"
 done
